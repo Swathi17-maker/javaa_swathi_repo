7bed1f72745b8e16199af7840b58cf856565ddef
Swathi17-maker <swathi1762000@gmail.com>
2025-06-30 18:44:51 +0530

    evennumbers
    for practice

---
+++ EvenNumbers.java
@@ -0,0 +1,49 @@
+/*class EvenNumbers //Print the even numbers from 1 to 100 using while loop
+{
+ public static void main(String[] args)
+ {
+  int a = 1;
+  while(a<=100)
+  {
+   if(a%2==0)
+   System.out.println(a);
+   a++;
+  }
+ }
+}*/
+
+
+
+/*class EvenNumbers //Print the even numbers from 1 to 100 using for loop
+{
+ public static void main(String[] args)
+ {
+  for(int a=1;a<=100;)
+   {
+    if(a%2==0)
+      System.out.println(a);
+      a++;
+   }
+ }
+}*/
+
+
+
+class EvenNumbers
+{
+ public static void main(String[] args)
+ {
+ int a = 1;
+   do
+    {
+     if(a%2==0)
+     System.out.println(a);
+     a++;
+    }
+   while(a<=100);
+  }
+}
+
+
+
+  
