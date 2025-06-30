bc55428ab20009ea2219c16d4ea882e39325685f
Swathi17-maker <swathi1762000@gmail.com>
2025-06-30 07:50:43 +0530

    temperature
    for practice

---
+++ Temperature.java
@@ -0,0 +1,13 @@
+class Temperature
+{
+ public static void main(String[] args)
+ {
+  float celsius = 45.6f;
+
+  float fahrenheit = ((celsius*1.8f)+32);
+
+  System.out.println("Temperature in Fahrenheit is " + fahrenheit );
+ }
+
+}
+  
