950ca173c3c73c6d9697a848b88702b580fc1d61
Swathi17-maker <swathi1762000@gmail.com>
2025-07-01 07:51:46 +0530

    odd numbers
    practice

---
+++ OddNumbers.java
@@ -0,0 +1,48 @@
+/*class OddNumbers //to print odd numbers from 1 to 100 using for loop
+{
+  public static void main(String[] args)
+   {
+    for(int a=1;a<=100;a++)
+    {
+     if(a%2==1)
+     System.out.println(a);
+    }
+   }
+}*/
+
+
+
+/*class OddNumbers //to print odd numbers from 1 to 100 using while loop
+{
+  public static void main(String[] args)
+  {
+   int a=1;
+   while(a<=100)
+   {
+    if(a%2==1)
+    System.out.println(a);
+    a++;
+   }
+  }
+}*/
+
+
+class OddNumbers   //to print odd numbers from 1 to 100 using do/while loop
+{
+ public static void main(String[] args)
+ {
+  int a=1;
+  do
+    {
+     if(a%2==1)
+     System.out.println(a);
+     a++;
+    }
+  while(a<=100);
+ }
+}
+    
+     
+     
+
+   
