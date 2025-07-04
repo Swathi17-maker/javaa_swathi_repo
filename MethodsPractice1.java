4377bc4a5fdbcf64aadd3e3d1b01247eef29ba4a
Swathi17-maker <swathi1762000@gmail.com>
2025-07-04 06:21:41 +0530

    Update MethodsPractice1.java
    for practice

--- MethodsPractice1.java
+++ MethodsPractice1.java
@@ -1,9 +1,6 @@
 import java.util.Scanner;
-
 class MethodsPractice                       //Write a java program that should have 3 methods To find average of 3 numbers
 {
-
-
  public void average(int a,int b,int c)
  {
   float d=(a+b+c)/3f;
@@ -27,26 +24,46 @@ class MethodsPractice                       //Write a java program that should h
   System.out.println(c + " is odd");
   else
   System.out.println(c + " is even");
-  
  }
 
 
-
-
- public void range(int a,int b,int c)
+ public void range(int a,int b)
  {
   int start=a;
   int end =b;
-  int d = 2;
   System.out.println("The range between:" + a + "-" + b);
-  for(int i = start;i<=end;i=i+d)
+  for(;a<=b;a++)                              //using for loop
   {
-    System.out.println(i);                    // the range between 10-20 is 
+    System.out.println(a);                    // the range between 10-20 is 
   }
  }
+ 
+/*public void range(int a, int b)
+{
+ int start = a; //10
+ int end = b;   //20
+ System.out.println("The range between: " + a + "-" + b); //Then range between: 10-20
+ while(a<=end)   //10<=20                        //using while loop
+ {
+   System.out.println(a);
+   a++;
+ }
+}*/
+ 
+/*public void range(int a,int b)
+{
+ int start = a; //10
+ int end = b;   //20
+ System.out.println("The range between: " + a + "-" + b); //Then range between: 10-20
+ do                                                   //using do/while loop
+   {
+    System.out.println(a);
+    a++;
+   }
+while(a<=b);
+}*/
 
-
-  
+ 
 public static void main(String[] args)
  {
   Scanner myobj = new Scanner(System.in);       //to get input from the user
@@ -58,7 +75,7 @@ public static void main(String[] args)
   MethodsPractice demo = new MethodsPractice();     // classname methodsname = new classname
   demo.average(a,b,c);                               // methodname.method
   demo.oddoreven(a,b,c);                             // to call oddorevenmethod
-  demo.range(a,b,c); 
+  demo.range(a,b); 
  }
 }
 
